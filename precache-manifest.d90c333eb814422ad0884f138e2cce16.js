3b56262e1af749bbf69600b33d7911d1f579a858
arun-kushwaha04 <arunkushwaha89765@gmail.com>
2021-06-30 12:37:50 +0530

    Updates

---
+++ precache-manifest.d90c333eb814422ad0884f138e2cce16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac6cd8eb308b2ce985c419bef41d6e9e",
+    "url": "/Menu-React-Project/index.html"
+  },
+  {
+    "revision": "20a25d1890863b2e49e4",
+    "url": "/Menu-React-Project/static/css/main.26e112f5.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/Menu-React-Project/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Menu-React-Project/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20a25d1890863b2e49e4",
+    "url": "/Menu-React-Project/static/js/main.01af5e5f.chunk.js"
+  },
+  {
+    "revision": "061ea06046c480be13ba",
+    "url": "/Menu-React-Project/static/js/runtime-main.eef8cef3.js"
+  }
+]);
